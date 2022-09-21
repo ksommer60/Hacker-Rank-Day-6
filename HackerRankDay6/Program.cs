d6d7939bc72c4b9a2922d9c3a81561c5e17ea49a
ksommer60 <105603761+ksommer60@users.noreply.github.com>
2022-09-21 07:54:02 -0400

    Add files via upload

---
+++ HackerRankDay6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HackerRankDay6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var N = int.Parse(Console.ReadLine());
+
+            for (var i = 0; i < N; i++)
+            {
+                var s = Console.ReadLine();
+
+                for (var j = 0; j < s.Length; j++)
+                {
+                    if (j % 2 == 0)
+                    {
+                        Console.Write(s[j]);
+                    }
+                }
+
+                Console.Write(" "); 
+
+                for (var j = 0; j < s.Length; j++)
+                {
+                    if (j % 2 != 0)
+                    {
+                        Console.Write(s[j]);
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
